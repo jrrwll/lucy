bcfc75230afb6bbf668a5bcebcf2c534a78c63e3
tuke <jrriwll@gmail.com>
2026-02-26 23:01:53 +0800

    commit

--- lucy-api/src/test/java/org/dreamcat/lucy/service/CodeServiceTest.java
+++ lucy-api/src/test/java/org/dreamcat/lucy/service/CodeServiceTest.java
@@ -9,6 +9,7 @@ import org.junit.jupiter.api.Test;
 /**
  * @author Jerry Will
  * @version 2025-11-05
+ * @see <a href="https://github.com/CesiumLabs/code-examples/tree/main/codes">...</a>
  */
 public class CodeServiceTest extends BaseTest {
 
